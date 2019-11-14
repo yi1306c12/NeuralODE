df097e73ac7299472914206953d13a5e4d4662e9
Yuta Inoue <yi1306c12@gmail.com>
2019-11-14 18:56:37 +0900

    run experiment

--- msurtsukov/experiment.bash
+++ msurtsukov/experiment.bash
@@ -6,4 +6,4 @@ DATE=$(date +%Y%m%d_%H%M%S)
 git rev-parse HEAD
 #seq -w $X | xargs -P0 -IXXX python3 $SCRIPT --save_path $SAVEPATH"/"$DATE"/"XXX
 mkdir -p $SAVEPATH"/"$DATE
-seq $X | parallel --no-notice --joblog $SAVEPATH"/"$DATE"/parallel.log" --eta python3 $SCRIPT --save_path $SAVEPATH"/"$DATE"/"{#} --n_steps 5000
+seq $X | parallel --no-notice --joblog $SAVEPATH"/"$DATE"/parallel.log" --eta python3 $SCRIPT --save_path $SAVEPATH"/"$DATE"/"{#} --n_steps 10000
